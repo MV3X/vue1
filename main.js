472f336a6bcee54029b73c9d842badf5a1375589
MV3X <mv3x@.ru>
2023-03-01 03:12:40 +0500

    save

---
+++ main.js
@@ -0,0 +1,22 @@
+const app = {
+    data() {
+        return {
+            color: 'white',
+            write: '',
+            info: '',
+            number: 0,
+            number2: 20
+
+        }
+    },
+    // methods: {
+    //     addColor(e) {
+    //         e.innerHTML = e
+    //         this.red
+    //         this.green 
+    //         this.orange
+    //     }
+    // }
+}
+
+Vue.createApp(app).mount('#app')
